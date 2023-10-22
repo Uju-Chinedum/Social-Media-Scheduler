5141fffa6cc77b45e492a9509276a84860adf674
Savadow <ujuchimaraoke@gmail.com>
2023-10-22 13:40:01 +0100

    feat: added errors

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const Unauthenticated = require("./Unauthenticated");
+const NotFound = require("./NotFound");
+const BadRequest = require("./BadRequest");
+const Unauthorized = require("./Unauthorized");
+
+module.exports = {
+  Unauthenticated,
+  NotFound,
+  BadRequest,
+  Unauthorized,
+};
