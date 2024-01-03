ab688e581f7116365cc83a135ae4c5e4cedcf18c
Savadow <ujuchimaraoke@gmail.com>
2024-01-03 21:37:12 +0100

    feat: completed getAllPosts

--- controllers/postController.js
+++ controllers/postController.js
@@ -52,16 +52,34 @@ const createPost = async (req, res) => {
     user.numOfPosts++;
     await user.save();
 
-    res.status(StatusCodes.CREATED).json({ message: "Post created successfully", post });
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: "Post created successfully", post });
   });
 };
 
 const getAllPosts = async (req, res) => {
-  res.send("get all posts");
+  const queryObject = {
+    user: req.user.userId,
+  };
+
+  let result = Post.find(queryObject);
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const posts = await result;
+
+  const totalPosts = await Post.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalPosts / limit);
+
+  res.status(StatusCodes.OK).json({ posts, totalPosts, numOfPages });
 };
 
 const getSinglePost = async (req, res) => {
-  res.send("get single post");
+  const 
 };
 
 const updatePost = async (req, res) => {
