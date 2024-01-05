f3e6fd36c23bbf74ee6b4901577710cb96ff69ae
Savadow <ujuchimaraoke@gmail.com>
2024-01-05 10:30:12 +0100

    refactor(error): wrapped error into data object

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,33 +1,34 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandler = (err, req, res, next) => {
-  let customError = {
+  let customError = {};
+  customError.data = {
     status: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something went wrong!! Please try again.",
   };
 
   if (err.name === "ValidationError") {
-    customError.message = Object.values(err.errors)
+    customError.data.message = Object.values(err.errors)
       .map((item) => item.message)
       .join(", ");
-    customError.status = StatusCodes.BAD_REQUEST;
+    customError.data.status = StatusCodes.BAD_REQUEST;
   }
 
   if (err.code && err.code === 11000) {
-    customError.message = `This ${Object.keys(
+    customError.data.message = `This ${Object.keys(
       err.keyValue
     )} is already used by a user. Please use another ${Object.keys(
       err.keyValue
     )}.`;
-    customError.status = StatusCodes.BAD_REQUEST;
+    customError.data.status = StatusCodes.BAD_REQUEST;
   }
 
   if (err.name === "CastError") {
-    customError.message = `No item found with id: ${err.value}`;
-    customError.status = StatusCodes.NOT_FOUND;
+    customError.data.message = `No item found with id: ${err.value}`;
+    customError.data.status = StatusCodes.NOT_FOUND;
   }
 
-  return res.status(customError.status).json(customError);
+  return res.status(customError.data.status).json(customError);
 };
 
 module.exports = errorHandler;
