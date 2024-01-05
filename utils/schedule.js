bfd5edae6c68ad966f559f03be0c10e1538b904c
Savadow <ujuchimaraoke@gmail.com>
2024-01-05 07:17:01 +0100

    feat: optimizing code

--- utils/schedule.js
+++ utils/schedule.js
@@ -2,6 +2,7 @@ const schedule = require("node-schedule");
 const IgApiClient = require("instagram-private-api");
 const Post = require("./models/post");
 const User = require("./models/user");
+const fs = require("fs"); // Added for stream handling
 
 const createScheduler = async () => {
   const ig = new IgApiClient();
@@ -14,11 +15,11 @@ const createScheduler = async () => {
       scheduledDate: { $lte: now },
       scheduledTime: { $lte: now.toLocaleTimeString() },
       isPosted: false,
-    });
+    }).populate("user"); // Populate user data to reduce queries
 
     for (const post of posts) {
       try {
-        const user = await User.findById(post.user);
+        const user = await User.findById(post.user); // Might still be needed for authentication
         await ig.state.restore(user.igUsername);
 
         if (
@@ -30,13 +31,15 @@ const createScheduler = async () => {
         } else {
           const mediaPromises = post.media.map(async (mediaItem) => {
             if (mediaItem.type === "image") {
+              const stream = fs.createReadStream(mediaItem.filePath); // Use stream for images
               return await ig.publish.uploadPhoto({
-                filePath: mediaItem.filePath,
+                stream,
                 caption: post.content,
               });
             } else if (mediaItem.type === "video") {
+              const stream = fs.createReadStream(mediaItem.filePath);
               return await ig.publish.uploadVideo({
-                filePath: mediaItem.filePath,
+                stream,
                 caption: post.content,
               });
             }
